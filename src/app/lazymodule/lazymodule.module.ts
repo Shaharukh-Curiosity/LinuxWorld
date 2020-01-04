5eb74cb71a9e7310acc0c78a8ad2671224574569
Shaharukh Parmar <shaharukh.rsp786@gmail.com>
2020-01-03 21:29:10 -0500

    modified privacy.html and recompile to validate the error

--- src/app/lazymodule/lazymodule.module.ts
+++ src/app/lazymodule/lazymodule.module.ts
@@ -11,7 +11,7 @@ const routes: Routes=[
 {path:'aboutus', component:AboutusComponent},
 {path:'feedback', component:FeedbackComponent},
 {path:'sitemap', component:SitemapComponent},
-{path:'privacy',component:PrivacyComponent}
+{path:'privacy-terms',component:PrivacyComponent}
 //{path:'**', component:ErrorComponent}
 
 ]
