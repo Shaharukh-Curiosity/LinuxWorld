f6bb40e8c0321e740194fd424dca3077ee14b0d0
Shaharukh Parmar <shaharukh.rsp786@gmail.com>
2020-05-12 10:18:05 +0530

    made changes

---
+++ dist/static.paths.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ROUTES = [
+    '/',
+    '/ubuntu-19.04-installation',
+    '/ubuntu-nodejs-installation',
+    '/ubuntu-openssh',
+    '/sitemap',
+    '/aboutus',
+    '/privacy-terms',
+    '/ubuntu-tutorials',
+    '/feedback',
+    '/detecting-open-port-linux',
+    '/create-non-root-user-on-kali',
+    '/partition-filesystem-on-linux',
+    '/10-basic-linux-commands',
+    '/how-to-install-ubuntu-20.04'
+];
+//# sourceMappingURL=static.paths.js.map
