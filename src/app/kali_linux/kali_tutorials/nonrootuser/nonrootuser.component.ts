75775bce754ff59cdd2570bad2b7434a4d56057d
Shaharukh Parmar <shaharukh.rsp786@gmail.com>
2020-07-14 16:57:05 +0530

    aadded disqus comment apis to get user feeback to take site next level

--- src/app/kali_linux/kali_tutorials/nonrootuser/nonrootuser.component.ts
+++ src/app/kali_linux/kali_tutorials/nonrootuser/nonrootuser.component.ts
@@ -10,6 +10,24 @@ export class NonrootuserComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    
+    /**
+    *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+    *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+    
+    var disqus_config = function () {
+      this.page.url = 'https://visionrosetta.in/create-non-root-account-on-kali';  // Replace PAGE_URL with your page's canonical URL variable
+      this.page.identifier ='create-non-root-account-on-kali'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+      };
+      
+      (function() { // DON'T EDIT BELOW THIS LINE
+      var d = document, s = d.createElement('script');
+      s.src = 'https://https-visionrosetta-in-3.disqus.com/embed.js';
+      s.setAttribute('data-timestamp', + new Date());
+      (d.head || d.body).appendChild(s);
+      })();
+      
+     
   }
 
 }
