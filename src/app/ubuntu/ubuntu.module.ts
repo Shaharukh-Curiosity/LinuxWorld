2d9421325380ea23555049e7826f37dd670a12c7
Shaharukh Parmar <shaharukh.rsp786@gmail.com>
2020-12-13 14:26:15 +0530

    modified some files and updated the sitmap.xml

--- src/app/ubuntu/ubuntu.module.ts
+++ src/app/ubuntu/ubuntu.module.ts
@@ -7,12 +7,8 @@ import{DetectingOpenportComponent} from './Tutorials/detecting-openport/detectin
 import {OpensshComponent}from './Tutorials/openssh/openssh.component';
 import { PartitionFilesystemComponent } from './Tutorials/partition-filesystem/partition-filesystem.component';
 import { Ubuntu20Component } from './Tutorials/ubuntu20/ubuntu20.component';
-<<<<<<< HEAD
 import { UbuntuLoginLoopProblemComponent } from './Tutorials/ubuntu-login-loop-problem/ubuntu-login-loop-problem.component';
  const routes: Routes=[
-=======
-const routes: Routes=[
->>>>>>> 1ec4c86735c319ace676d54b3862a1df3224f11e
 
     {path:'ubuntu-19.04-installation', component:UbuntuinstalltionComponent},
     {path:'ubuntu-tutorials', component:UbuntuTutHomeComponent},
@@ -28,22 +24,16 @@ const routes: Routes=[
 
     {path:'how-to-install-ubuntu-20.04',
      component:Ubuntu20Component
-<<<<<<< HEAD
     } ,
+    
     {
         path:'ubuntu-20.4-login-loop-problem',
         component:UbuntuLoginLoopProblemComponent
-=======
->>>>>>> 1ec4c86735c319ace676d54b3862a1df3224f11e
     }
 ]
 
 @NgModule({
-<<<<<<< HEAD
     declarations:[UbuntuTutHomeComponent,UbuntuinstalltionComponent,DetectingOpenportComponent, OpensshComponent, PartitionFilesystemComponent, Ubuntu20Component, UbuntuLoginLoopProblemComponent],
-=======
-    declarations:[UbuntuTutHomeComponent,UbuntuinstalltionComponent,DetectingOpenportComponent, OpensshComponent, PartitionFilesystemComponent, Ubuntu20Component],
->>>>>>> 1ec4c86735c319ace676d54b3862a1df3224f11e
     imports:[
         CommonModule,
         RouterModule.forChild(routes)
--- static.paths.ts
+++ static.paths.ts
@@ -15,11 +15,8 @@ export const ROUTES = [
   '/how-to-install-ubuntu-20.04',
   '/introduction-linux-shell-scripting',
   '/kali-linux-tutorials',
-<<<<<<< HEAD
   '/shell-script-tutorials',
-  '/ubuntu-20.4-login-loop-problem'
-=======
+  '/ubuntu-20.4-login-loop-problem',
   '/shell-script-tutorials'
->>>>>>> 1ec4c86735c319ace676d54b3862a1df3224f11e
 ];
  
