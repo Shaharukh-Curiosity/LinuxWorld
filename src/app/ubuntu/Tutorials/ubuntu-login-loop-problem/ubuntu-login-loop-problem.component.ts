951a99588e7381073bf0779fd4994a0533e3a916
Shaharukh Parmar <shaharukh.rsp786@gmail.com>
2020-12-13 10:18:25 +0530

    added new post to blog(ubuntu20.04 login loop problem)

---
+++ src/app/ubuntu/Tutorials/ubuntu-login-loop-problem/ubuntu-login-loop-problem.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ubuntu-login-loop-problem',
+  templateUrl: './ubuntu-login-loop-problem.component.html',
+  styleUrls: ['./ubuntu-login-loop-problem.component.css']
+})
+export class UbuntuLoginLoopProblemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
